ab4cfd4edd72b2ec0dab4394f7a027ca4e544e01
CipherX <49910170+TheCipherX@users.noreply.github.com>
2020-05-31 14:06:48 +0530

    May31

---
+++ install.sh
@@ -0,0 +1,29 @@
+#! /$HOME/
+#---CipherBoT--One--Click--UPDATE-------------
+echo "" #blank
+echo "" #blank
+echo "" #blank
+echo "" #blank
+#logo of this script
+figlet "CIPHER BOT"
+echo "---T34M 4N0NYM0U5---"
+echo "---Creator: Khalid Sha [CIPHER-X]--"
+echo "---Subscribe CN HACKERs on YouTube 4 More Updates--"
+echo "---Version: 3.2----"
+echo "" #blank
+
+#git pkg
+echo "Checking & Removing Junk Files......"
+rm -r cipherbot
+echo "Installing NodeJS for CipherBot Support...."
+apt install nodejs -y
+echo "Installing CipherBot......"
+git clone https://github.com/thecipherx/cipherbot
+
+
+#end
+echo "####################################################################"
+echo "--------------------------------------------------------------------"
+echo "---------SUCCESSFULY UPDATED CIPHER BOT---------------------------"
+echo "---------------------by KHALID SHA----------------------------------"
+echo "###################################################################"
